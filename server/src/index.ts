eb7e6dbf8fb380aaa7e742c5926a93c1462d87a4
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-31 15:01:23 -0700

    Create server file and move all relevant files there

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+// import { MikroORM, RequiredEntityData } from "@mikro-orm/core";
+// import { __prod__ } from "./constants";
+// import { Post } from "./entities/Post";
+// import microConfig from "./mikro-orm.config";
+
+(async () => {
+	// const orm = await MikroORM.init(microConfig);
+	console.log("Hello World");
+
+	// const post = orm.em.create(Post, {
+	// 	title: "First Post!",
+	// } as RequiredEntityData<Post>);
+	// await orm.em.persistAndFlush(post);
+})();
