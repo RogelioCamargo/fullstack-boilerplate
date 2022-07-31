cd00fac67d3e89f51991703bec8a43992ae1526a
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-31 10:53:05 -0700

    Set up mirco-orm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import { MikroORM, RequiredEntityData } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+(async () => {
+	const orm = await MikroORM.init(microConfig);
+
+	// const post = orm.em.create(Post, {
+	// 	title: "First Post!",
+	// } as RequiredEntityData<Post>);
+	// await orm.em.persistAndFlush(post);
+})();
