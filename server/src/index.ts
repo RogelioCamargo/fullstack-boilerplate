539e9ba29d4ae4cfbe0a7c97a7cf6af7cf89c6ab
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-07-31 18:28:10 -0700

    Set up graphql server and example resolver

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,30 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 (async () => {
 	const orm = await MikroORM.init(microConfig);
-	// console.log("Hello World");
 	await orm.getMigrator().up();
+
+	const app = express();
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver],
+			validate: false,
+		}),
+	});
+
+	await apolloServer.start();
+	apolloServer.applyMiddleware({ app });
+
+	app.listen(5000, () => {
+		console.log("Server started on localhost:5000");
+	});
 
 	// const post = orm.em.create(Post, {
 	// 	title: "First Post!",
