241c8da8da2a01062b369b714d39565b08213441
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-08-02 19:03:47 -0700

    Create login method for user resolver

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,5 +1,13 @@
 import { MyContext } from "src/types";
-import { Resolver, Mutation, Arg, InputType, Field, Ctx } from "type-graphql";
+import {
+	Resolver,
+	Mutation,
+	Arg,
+	InputType,
+	Field,
+	Ctx,
+	ObjectType,
+} from "type-graphql";
 import { User } from "../entities/User";
 import argon2 from "argon2";
 
@@ -9,6 +17,24 @@ class UsernamePasswordInput {
 	username: string;
 	@Field()
 	password: string;
+}
+
+@ObjectType()
+class FieldError {
+	@Field()
+	field: string;
+
+	@Field()
+	message: string;
+}
+
+@ObjectType()
+class UserResponse {
+	@Field(() => [FieldError], { nullable: true })
+	errors?: FieldError[];
+
+	@Field(() => User, { nullable: true })
+	user?: User;
 }
 
 @Resolver()
@@ -26,5 +52,34 @@ export class UserResolver {
 		});
 		await fork.persistAndFlush(user);
 		return user;
+	}
+
+	@Mutation(() => UserResponse)
+	async login(
+		@Arg("options") options: UsernamePasswordInput,
+		@Ctx() { em }: MyContext
+	): Promise<UserResponse> {
+		const fork = em.fork();
+		const user = await fork.findOne(User, {
+			username: options.username,
+		});
+		if (!user) {
+			return {
+				errors: [
+					{
+						field: "username",
+						message: "username does not exist",
+					},
+				],
+			};
+		}
+		const valid = await argon2.verify(user.password, options.password);
+
+		if (!valid) {
+			return {
+				errors: [{ field: "password", message: "password is not correct" }],
+			};
+		}
+		return { user };
 	}
 }
