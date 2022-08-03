10f434fe1ea4dd30c0a38bde277574dcc52765d6
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-08-02 19:14:39 -0700

    Add input validators for register method

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -39,19 +39,40 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-	@Mutation(() => User)
+	@Mutation(() => UserResponse)
 	async register(
 		@Arg("options") options: UsernamePasswordInput,
 		@Ctx() { em }: MyContext
-	) {
+	): Promise<UserResponse> {
 		const fork = em.fork();
+		if (options.username.length <= 3) {
+			return {
+				errors: [
+					{
+						field: "username",
+						message: "lenth must be greater than 3",
+					},
+				],
+			};
+		}
+
+		if (options.password.length <= 3) {
+			return {
+				errors: [
+					{
+						field: "password",
+						message: "length must be greater than 3",
+					},
+				],
+			};
+		}
 		const hashedPassword = await argon2.hash(options.password);
 		const user = fork.create(User, {
 			username: options.username,
 			password: hashedPassword,
 		});
 		await fork.persistAndFlush(user);
-		return user;
+		return { user };
 	}
 
 	@Mutation(() => UserResponse)
